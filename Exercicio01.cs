feb28063b9e56b1156a7207eb169d7a83d625558
SamuelHansen1107 <samuelhansen07@hotmail.com>
2026-09-17 06:38:48 -0300

    feat: adiciona exercicios de laços

---
+++ Exercicio01.cs
@@ -0,0 +1,17 @@
+/*
+// digitar um número e listar todos até ele
+int numero;
+
+Console.Write("Digite um número: ");
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    for (int i = 1; i <= numero; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("Valor inválido!");
+}
+*/
---
+++ Exercicio02.cs
@@ -0,0 +1,25 @@
+/*
+// mostrar numeros pares e impares até N
+
+int numero;
+
+Console.Write("Digite um numero: ");
+if (int.TryParse(Console.ReadLine(), out numero))
+{
+    for (int i = 0; i <= numero; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i+" ");
+        }
+    }
+    Console.WriteLine();
+    for (int i = 0; i <= numero; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.Write(i+" ");
+        }
+    }
+}
+*/
